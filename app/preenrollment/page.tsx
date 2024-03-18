426a0a82f329cfe565812803a45de3185fb71f6a
samsonwong522 <samsonwong522@gmail.com>
2024-03-18 18:55:57 +0800

    Handle course prerequisites and exclusions in pre-enrollment page

--- app/preenrollment/page.tsx
+++ app/preenrollment/page.tsx
@@ -2,25 +2,71 @@
 
 import Accordion from "@/components/accordion";
 import Chip from "@/components/chip";
+import {
+  COURSE_CATALOG,
+  checkExclusionGroup,
+  checkPrerequisiteGroup,
+} from "@/helpers/course";
 import { useAppSelector } from "@/redux/hooks";
 
 export default function PreEnrollment() {
   // TODO: Validate if all the requirements are required, not electives
-  const requirementGroups = useAppSelector((state) =>
-    state.plannerReducer.selectedDegrees.flatMap((degree) =>
-      degree.requirements
-        .filter((requirement) =>
-          ["major", "school"].includes(requirement.rules.type),
-        )
-        .flatMap((requirement) =>
-          requirement.requirementGroups.filter(
-            (requirement) =>
-              !requirement.name.includes("Elective") &&
-              !requirement.name.includes("Common Core"),
-          ),
-        ),
-    ),
+  const degrees = useAppSelector(
+    (state) => state.plannerReducer.selectedDegrees,
   );
+
+  const requirementGroups = degrees.flatMap((degree) =>
+    degree.requirements
+      .filter((requirement) =>
+        ["major", "school"].includes(requirement.rules.type),
+      )
+      .flatMap((requirement) =>
+        requirement.requirementGroups.filter(
+          (requirement) =>
+            !requirement.name.includes("Elective") &&
+            !requirement.name.includes("Common Core"),
+        ),
+      ),
+  );
+
+  const courseHistory = useAppSelector((state) =>
+    state.courseReducer.courseHistory.map((course) => course.code),
+  );
+
+  const CourseChip = ({ course }: { course: string }) => {
+    const taken = courseHistory.includes(course);
+
+    if (taken) {
+      return (
+        <Chip
+          label={course}
+          color="gray"
+          className={taken ? "line-through" : ""}
+        />
+      );
+    }
+
+    const courseDetail = COURSE_CATALOG.find((c) => c.code === course);
+    if (!courseDetail) {
+      return <Chip label={course} />;
+    }
+
+    const excluded = checkExclusionGroup(courseDetail, courseHistory);
+    if (excluded) {
+      return <Chip label={course} color="red" className={"line-through"} />;
+    }
+
+    const fulfilledPreRequisites = checkPrerequisiteGroup(
+      courseDetail,
+      courseHistory,
+    );
+
+    if (fulfilledPreRequisites) {
+      return <Chip label={course} color="green" />;
+    }
+
+    return <Chip label={course} color="red" />;
+  };
 
   return (
     <div>
@@ -44,9 +90,9 @@ export default function PreEnrollment() {
                       {requirement.name}
                     </h4>
                     <div className="flex flex-wrap">
-                      {courses.map((course) => (
-                        <Chip key={course} label={course} />
-                      ))}
+                      {courses.map((course) => {
+                        return <CourseChip key={course} course={course} />;
+                      })}
                     </div>
                   </div>
                 );
